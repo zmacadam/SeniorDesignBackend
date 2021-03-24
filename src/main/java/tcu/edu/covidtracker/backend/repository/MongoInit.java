1602b0d7c23bf103b8d6a00909c4ee86da24c3f4
Zach Macadam <z.macadam@tcu.edu>
2021-03-23 19:52:44 -0500

    Fixed MongoInit configuration.

--- src/main/java/tcu/edu/covidtracker/backend/repository/MongoInit.java
+++ src/main/java/tcu/edu/covidtracker/backend/repository/MongoInit.java
@@ -3,13 +3,16 @@ package tcu.edu.covidtracker.backend.repository;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MongoInit {
 
-    private MongoClient mongoClient = new MongoClient("localhost", 27017);
-    private DB db = mongoClient.getDB("CovidTracker");
+    MongoClientURI uri = new MongoClientURI(
+            "mongodb://root:4x7NOpp0OFnpl1qD@cluster0-shard-00-00.i4jnr.mongodb.net:27017,cluster0-shard-00-01.i4jnr.mongodb.net:27017,cluster0-shard-00-02.i4jnr.mongodb.net:27017/CovidTracker?ssl=true&replicaSet=atlas-11ucwv-shard-0&authSource=admin&retryWrites=true&w=majority");
+    MongoClient mongoClient = new MongoClient(uri);
+    DB db = mongoClient.getDB("CovidTracker");
 
     public DBCollection getCollection(String name) {
         return db.getCollection(name);
